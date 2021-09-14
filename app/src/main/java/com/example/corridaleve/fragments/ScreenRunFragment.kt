e5a0aa97d95e0e91ea5ddeda121574c00a29c9cb
samuellchagas <samuelltaylor@gmail.com>
2021-09-14 13:50:39 -0300

    Localização em tempo real do usuario ataulizada a cada 5 segundos.

--- app/src/main/java/com/example/corridaleve/fragments/ScreenRunFragment.kt
+++ app/src/main/java/com/example/corridaleve/fragments/ScreenRunFragment.kt
@@ -1,7 +1,9 @@
 package com.example.corridaleve.fragments
 
+import android.Manifest
 import android.app.AlertDialog
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -11,26 +13,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.corridaleve.R
 import com.example.corridaleve.databinding.ScreenRunFragmentBinding
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import java.text.DecimalFormat
 import java.util.*
 import java.util.zip.Inflater
 
-class ScreenRunFragment:Fragment(),LocationListener {
+class ScreenRunFragment:Fragment() {
 
     private var _binding: ScreenRunFragmentBinding? = null
     private val binding:ScreenRunFragmentBinding get() = _binding!!
 
+
     val time: Long = 1000000000L
     var timer = Timer(time)
 
-    private var locationManager:LocationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-    private var locationListener = LocationListener{}
-    private var listLocation:List<String> = listOf()
+
+    //private val locationListener:LocationListener()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,29 +53,36 @@ class ScreenRunFragment:Fragment(),LocationListener {
 
         timer.start()
 
+        var locationManager:LocationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
         binding.imageViewStop.setOnClickListener {
             timer.cancel()
-            val timing:Long =1000000000-timer.millisUntilFinished
-            Toast.makeText(requireContext(),formatTime(timing),Toast.LENGTH_LONG).show()
+            val timing: Long = 1000000000 - timer.millisUntilFinished
+            Toast.makeText(requireContext(), formatTime(timing), Toast.LENGTH_LONG).show()
             //requireActivity().finish()
-
-//            locationManager.requestLocationUpdates(
-//                LocationManager.GPS_PROVIDER,
-//                5000,
-//                5,
-//                locationListener
-//
-//            )
-
-//            locationManager.requestLocationUpdates(
-//                LocationManager.GPS_PROVIDER,
-//                0,
-//                0,
-//                LocationListener {  }
-//            )
         }
-    }
 
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                (requireContext()),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
+        }
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            5000,
+            0F,
+            object:LocationListener {
+                override fun onLocationChanged(p0: Location) {
+                    Toast.makeText(requireContext(), p0.longitude.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
     inner class Timer(miliis:Long) : CountDownTimer(miliis,1){
         var millisUntilFinished:Long = 0
         override fun onFinish() {
@@ -92,7 +106,5 @@ class ScreenRunFragment:Fragment(),LocationListener {
         return f.format(hour) + ":" + f.format(min) + ":" + f.format(sec)
     }
 
-    override fun onLocationChanged(p0: Location) {
-        Toast.makeText(requireContext(),"TESTE LOCALIZAÇÂO",Toast.LENGTH_LONG).show()
-    }
 }
+
